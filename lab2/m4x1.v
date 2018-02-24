56355785bd70a1e0423228102fb44af64abb37a5
Ryan Kozak <ryankozak@csus.edu>
2018-02-24 10:17:20 -0800

    multiplexer comments

--- lab2/m4x1.v
+++ lab2/m4x1.v
@@ -1,58 +1,59 @@
 // Ryan Kozak
 // CSC137 Class #32509
-// 
+// 2nd Programming Assignment
+//
 // m4x1.v, 4x1 multiplexor, gate synthesis
 // compile: ~changw/ivl/bin/iverilog m4x1.v
 // run: ./a.out
 
 
 
-module DecoderMod(s, o); // module definition
+module DecoderMod(s, o); // decoder module definition
 
-   input [1:0] s;
-   output [0:3] o;
+   input [1:0] s; // input array
+   output [0:3] o; // output array
 
-   wire [1:0] n_s;
+   wire [1:0] n_s; // extra wires
 
-   not(n_s[1], s[1]);
-   not(n_s[0], s[0]);  
+   not(n_s[1], s[1]); // first not gate in diagram
+   not(n_s[0], s[0]);  // second not gate in diagram
 
-   and(o[0], n_s[1], n_s[0]);
-   and(o[1], n_s[1], s[0]);
-   and(o[2], s[1], n_s[0]);
-   and(o[3], s[1], s[0]);
+   and(o[0], n_s[1], n_s[0]); // first and gate in diagram
+   and(o[1], n_s[1], s[0]); // second and gate in diagram
+   and(o[2], s[1], n_s[0]); // third and gate in diagram
+   and(o[3], s[1], s[0]);  // fourth and gate in diagram
 
 endmodule
 
 
 
-module MuxMod(s, d, o);
+module MuxMod(s, d, o); // multiplexer module definition
 
-   input [1:0] s;
-   input [0:3] d;
+   input [1:0] s; // input array
+   input [0:3] d; // input array
    
-   output o;
+   output o; // output
 
-   wire [0:3] io, w_and;
+   wire [0:3] io, w_and; // extra wires
 
-   DecoderMod my_decoder(s, io);
+   DecoderMod my_decoder(s, io); // declaration of decoder module
   
-   and(w_and[0], io[0], d[0] );
-   and(w_and[1], io[1], d[1]);
-   and(w_and[2], io[2], d[2]);
-   and(w_and[3], io[3], d[3]);
+   and(w_and[0], io[0], d[0] ); // first and gate in diagram
+   and(w_and[1], io[1], d[1]); // second and gate in diagram
+   and(w_and[2], io[2], d[2]); // third and gate in diagram
+   and(w_and[3], io[3], d[3]);  // fourth and gate in diagram
 
-   or(o, w_and[0], w_and[1], w_and[2], w_and[3]);
+   or(o, w_and[0], w_and[1], w_and[2], w_and[3]); // or gate
 
 endmodule
 
-module TestMod;
+module TestMod; // module to test multiplexer
 
-   reg [1:0] s;
+   reg [1:0] s; 
    reg [0:3] d;
    wire o;
 
-   MuxMod my_mux(s, d, o);
+   MuxMod my_mux(s, d, o); // delclaration of  multiplexer module
 
    initial begin
       $display("Time\ts\td\to");
