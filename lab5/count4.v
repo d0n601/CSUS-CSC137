06ba3294785603a8aa3e8da11d53b615a8f4d948
Ryan Kozak <ryankozak@csus.edu>
2018-04-06 08:01:12 -0700

    Lab5 count4.v completed.

--- lab5/count4.v
+++ lab5/count4.v
@@ -1,55 +1,68 @@
-// complete this code first
-// then expand it to count 0~15, and name it as count16.v
-// use only arrays for all registers and wires (except CLK)
-// Ryan Kozak CSC137
+// Ryan Kozak 
+// CSC137 Class #32509
+// 5th Programming Assignment
+//
+// count4.v, ripple counter
+// compile: ~changw/ivl/bin/iverilog count4.v
+// run: ./a.out 
 
 module TestMod;
+
    reg CLK;
-   wire Q0, Q1, Q2, Q3, C1, C0;
+   wire [0:3] Q;
+   wire [1:0] C; 
 
    always begin // this is clock wave
       CLK = 0;  // 0 for half cycle (#1)
       #1;
-      CLk = 1;  // 1 for half cycle (#1)
+      CLK = 1;  // 1 for half cycle (#1)
       #1;
    end
 
-   RippleMod my_ripple(... arguments ...)
-   CoderMod my_coder(... arguments ...)
+   RippleMod my_ripple(CLK, Q);
+   CoderMod my_coder(Q, C);
 
    initial #16 $finish;
 
    initial begin
-      $display("... header ...");
-      $monitor("... items ...", ...);
+      $display("Time\tCLK\tQ\tC");
+      $monitor("%4d\t%3d\t%b\t%b", $time, CLK, Q, C);
    end
 endmodule
 
-module CoderMod(4 unary input and 2 binary output);
-   input are these
-   output are these
-   logic gate statement to define an output from input
-   logic gate statement to define an output from input
+
+
+module CoderMod(Q, C);
+
+   input [0:3] Q;
+   output [1:0] C;
+
+   or(C[0], Q[1], Q[3]);
+   or(C[1], Q[2], Q[3]);
+
 endmodule
 
-module RippleMod(CLK, Q0, Q1, Q2, Q3);
-   input CLK;
-   output Q0, Q1, Q2, Q3;
 
-   reg Q0, Q1, Q2, Q3;
+
+module RippleMod(CLK, Q);
+  
+ input CLK;
+   output [0:3] Q;
+
+   reg [0:3] Q;
 
    always @(posedge CLK) begin
-      Q0 <= Q3;
-      Q1 <= Q0;
-      Q2 <= Q1;
-      Q3 <= Q2;
+      Q[0] <= Q[3];
+      Q[1] <= Q[0];
+      Q[2] <= Q[1];
+      Q[3] <= Q[2];
    end
 
    initial begin // here we conveniently set flipflops to 1000 (binary)
-      Q0 = 1;
-      Q1 = 0;
-      Q2 = 0;
-      Q3 = 0;
+      Q[0] = 1;
+      Q[1] = 0;
+      Q[2] = 0;
+      Q[3] = 0;
    end
 endmodule
 
