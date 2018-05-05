b1c2a719bb4469f89581e7bcb91b3c752fda8d83
Ryan Kozak <ryankozak@csus.edu>
2018-05-04 17:22:43 -0700

    Working Wc7-recog.v

--- lab7/name-recog.v
+++ lab7/name-recog.v
@@ -1,7 +1,10 @@
-// Wc7-recog.v
-// sequence recognizer, recognize input of "Wc7" by ouput match = 1
+// Ryan Kozak 
+// CSC137 Class #32509
 //
-// compile: ~changw/ivl/bin/iverilog Wc7-recog.v
+// 7th Programming Assignment
+// sequence recognizer, recognize input of "Ryan" by output match = 1
+//
+// compile: ~changw/ivl/bin/iverilog name-recog.v
 // run: ./a.out 
 
 module TestMod;
@@ -9,9 +12,8 @@ module TestMod;
    parameter STDIN = 32'h8000_0000; // keyboard-input file-handle address
 
    reg clk;
-   reg [6:0] str [1:4];  // to what's to be entered
+   reg [6:0] str [1:5];  // to what's to be entered
    wire match;           // to be set 1 when matched
-   
    reg [6:0] ascii;      // each input letter is an ASCII bitmap
 
    RecognizerMod my_recognizer(clk, ascii, match);
@@ -21,6 +23,8 @@ module TestMod;
       str[1] = $fgetc(STDIN);  // 1st letter
       str[2] = $fgetc(STDIN); // 2nd letter
       str[3] = $fgetc(STDIN); // 3rd letter
+      str[4] = $fgetc(STDIN); // 4th letter
+      str[5] = $fgetc(STDIN); // 5th letter (space)
 
       $display("Time clk    ascii       match");
       $monitor("%4d   %b    %c %b   %b", $time, clk, ascii, ascii, match);
@@ -30,10 +34,11 @@ module TestMod;
       ascii = str[1];
       #1 clk = 1; #1 clk = 0;
       ascii = str[2];
-      #1 clk = 1; #1 clk = 0;
+      #1 clk = 1; #1 clk = 0;		
       ascii = str[3];
       #1 clk = 1; #1 clk = 0;
-      
+      ascii = str[4];
+      #1 clk = 1;
    end
 
 endmodule
@@ -46,10 +51,10 @@ module RecognizerMod(clk, ascii, match);
    input [6:0] ascii;
    output match;
 
-   wire [0:2] Q [6:0];   // 3-input sequence, 7 bits each
-   wire [6:0] submatch;  // all bits matched (7 3-bit sequences)
+   wire [0:4] Q [0:6];   // 5-input sequence, 7 bits each
+   wire [6:0] submatch;  // all bits matched (7 5-bit sequences)
 
-   wire invQ60, invQ52, invQ41, invQ32, invQ31, invQ30; // inverted signals
+   wire [0:6] invQ;// inverted signals
 
    //         654 3210   Q
    //     hex binary
@@ -65,23 +70,17 @@ module RecognizerMod(clk, ascii, match);
    RippleMod Ripple1(clk, ascii[1], Q[1]);
    RippleMod Ripple0(clk, ascii[0], Q[0]);
 
-   not(invQ60, Q[6][0]);
-   not(invQ52, Q[5][2]);
-   not(invQ41, Q[4][1]);
-   not(invQ32, Q[3][2]);
-   not(invQ31, Q[3][1]);
-   not(invQ30, Q[3][0]);
-   not(invQ21, Q[2][1]);   
+   not(invQ[6], Q[6][0]);
+   not(invQ[5], Q[5][0]);
+   not(invQ[4], Q[4][0]);
+   not(invQ[3], Q[3][0]);
+   not(invQ[2], Q[2][0]);
+   not(invQ[1], Q[1][0]);
+   not(invQ[0], Q[0][0]);
 
-   and(submatch[6], Q[6][2], Q[6][1], invQ60);
-   and(submatch[5], invQ52, Q[5][1], Q[5][0]);
-   and(submatch[4], Q[4][2], invQ41, Q[4][0]);
-   and(submatch[3], invQ32, invQ31, invQ30);
-   and(submatch[2], Q[2][2], invQ21, Q[2][0]);
-   and(submatch[1], Q[1][2], Q[1][1], Q[1][0]);
-   and(submatch[0], Q[0][2], Q[0][1], Q[0][0]);
+   and(submatch[6], Q[6][4], Q[6][3], Q[6][2], Q[6][1], invQ[6]);
 
-   and(match, submatch[6], submatch[5], submatch[4], submatch[3], submatch[2], submatch[1], submatch[0]);
+   and(match, submatch[6]);
 
 endmodule
 
@@ -90,17 +89,19 @@ endmodule
 module RippleMod(clk, ascii_bit, q);
 
    input clk, ascii_bit;
-   output [0:2] q;
+   output [0:4] q;
 
-   reg [0:2] q;          // flipflops
+   reg [0:4] q;          // flipflops
 
    always @(posedge clk) begin
-      q[0] <= ascii_bit;
+      q[0] <= q[4];
       q[1] <= q[0];
       q[2] <= q[1];
+      q[3] <= q[2];
+      q[4] <= q[3];
    end
 
-   initial q = 3'b000;
+   initial q = 5'b00000;
 
 endmodule
 
